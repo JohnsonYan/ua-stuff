59f99728c4129b2bf64753b07c64e11381d3b0b8
yanjun <719162522@qq.com>
2017-08-09 19:50:30 -0700

    新增arpsniffer,源作者在调用pcap_next的时候并没有判断返回的包指针是否为空，造成了segment fault。修改之...

--- pcap/arpsniffer.c
+++ pcap/arpsniffer.c
@@ -32,5 +32,48 @@ int main(int argc, char *argv[])
     char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *descr = NULL;   // Network interface handler
     struct pcap_pkthdr pkthdr;  // Packet information (timestamp,size...)
+    const unsigned char *packet = NULL; // Received raw data
+    arphdr_t *arpheader = NULL;
 
+    memset(errbuf, 0, PCAP_ERRBUF_SIZE);
+
+    if (argc != 2) {
+        printf("USAGE: arpsniffer <interface>\n");
+        exit(1);
+    }
+    // open network device for packet capture
+    descr = pcap_open_live(argv[1], MAXBYTES2CAPTURE, 0, 512, errbuf);
+    // look up info from the capture device
+    pcap_lookupnet(argv[1], &netaddr, &mask, errbuf);
+    // compiles the fileter expression into a BPF filter program
+    pcap_compile(descr, &filter, "arp", 1, mask);
+    // load the filter program into the packet capture device
+    pcap_setfilter(descr, &filter);
+
+    while (1) {
+        if ((packet = pcap_next(descr, &pkthdr)) == NULL)
+            continue; // 出现错误或者没抓到包，返回NULL, 那就跳过这次循环, 防止出现空指针 
+        arpheader = (struct arphdr *)(packet+14); // point to the arp header
+
+        printf("\n\nReceived Packet Size: %d bytes\n", pkthdr.len);
+        
+        printf("Hardware type: %s\n", (ntohs(arpheader->htype) == 1) ? "Ethernet":"Unknown");
+        printf("Protocol type: %s\n", (ntohs(arpheader->ptype) == 0x0800) ? "IPv4":"Unknown");
+        printf("Operation: %s\n", (ntohs(arpheader->oper) == ARP_REQUEST) ? "ARP Request":"ARP Reply");
+
+        // if is Ethernet and IPv4, print packet contents
+        if (ntohs(arpheader->htype) == 1 && ntohs(arpheader->ptype) == 0x0800) {
+            printf("Sender MAC: ");
+            for (i = 0; i < 6; i++) printf("%02x:", arpheader->sha[i]);
+            printf("\nSender IP: ");
+            for (i = 0; i < 4; i++) printf("%d.", arpheader->spa[i]);
+            printf("\nTarget MAC: ");
+            for (i = 0; i < 6; i++) printf("%02x:", arpheader->tha[i]);
+            printf("\nTarget IP: ");
+            for (i = 0; i < 4; i++) printf("%d.", arpheader->tpa[i]);
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
