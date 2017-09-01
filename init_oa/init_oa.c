326a6ed85b394aea5e205ca29a674959de503d0e
yanjun <719162522@qq.com>
2017-08-31 21:25:01 -0700

    finishd init_oa 解析ua库的函数

--- init_oa/init_oa.c
+++ init_oa/init_oa.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 //-----------通用类型定义-------------------
 #define  UINT8     unsigned char
 #define  INT8      char
@@ -15,14 +17,16 @@
 #define UA_DEVICE_INFO 1
 #define UA_BROWSER_INFO 2
 #define UA_OS_INFO 3
+
 #define DEVICE_MOBILE 1
 #define DEVICE_TELEVISION 2
 #define DEVICE_TABLET 3
-#define DEVICE_EAEADER 4
+#define DEVICE_EREADER 4
 #define DEVICE_DESKTOP 5
-#define DEVICE_GAMEING 6
+#define DEVICE_GAMING 6
 #define DEVICE_MEDIA 7
-#define UA_OS_CFG "ua-os.cfg"				// 之前是UI_OS_CFG，我改成了UA_OS_CFG,下同
+
+#define UA_OS_CFG "ua-os.cfg"				
 #define UA_DEVICE_CFG "ua-device.cfg"
 #define UA_BROWSER_CFG "ua-browser.cfg"
 
@@ -32,18 +36,21 @@ typedef struct _os_device_browser
 	UINT8 type;							// UI_DEVICE_INFO, UI_BROWSER_INFO, UI_OS_INFO
 	UINT8 priority;                  	// 优先级
 	UINT8 content_len;               	// 长度都小于256
-	UINT8 os_brand_browser_len;			// 长度都小于256
+	UINT8 os_device_browser_len;			// 长度都小于256
 	INT8 *content;		             	// 待比较的内容
-	UINT8 *os_brand_browser;         	// 保存的是真正的浏览器，设备，操作系统字符串
-	struct _os_brand_browser * next;
+	UINT8 *os_device_browser;         	// 保存的是真正的浏览器，设备，操作系统字符串
+	struct _os_device_browser *next;
 }OS_DEVICE_BROWSER;
 
+//TODO 检查malloc申请成功与否
 OS_DEVICE_BROWSER  *init_os_device_browser()
 {
 	FILE *fp = NULL;
-	
-	char line[256] = {0};
-	char buf[256] = {0};
+	OS_DEVICE_BROWSER *list, *tmpt, *head;
+	char line[256];
+	int count = 0;	// 统计链表长度
+
+	// READ OS INFO
 	if(access(UA_OS_CFG, R_OK) == 0) {
 		if((fp = fopen(UA_OS_CFG, "r")) == NULL) {
 			fprintf(stderr, "Couldn't open the file %s\n", UA_OS_CFG);
@@ -51,47 +58,206 @@ OS_DEVICE_BROWSER  *init_os_device_browser()
 		}
 		char key[256];
 		char value[256];
+		
 
 		while(fgets(line, 256, fp) != NULL) {
-			// 初始化一个链表节点
-			OS_DEVICE_BROWSER *list = NULL;
-			list = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+			// 初始化一个链表节点,指向头结点head
+			if(++count == 1) {
+				list = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+				if(list == NULL) {
+					fprintf(stderr, "Memory allocation failed.");
+					return NULL;
+				}
+				head = list;
+			}
+			else {
+				tmpt = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+				if(tmpt == NULL) {
+					fprintf(stderr, "Memory allocation failed.");
+					return NULL;
+				}
+				list->next = tmpt;
+				tmpt->next = NULL;
+				list = tmpt;
+			}
+
+			sscanf(line, "%[^,]%*[,]%[^,]%*[,]%d", key, value, &list->priority);	// 读取出格式化数据
 
 			list->type = UA_OS_INFO;
-
-			sscanf(line, "%[^,]%*[,]%[^,]%d", key, value, &(list->priority));	// 读取出格式化数据
-
 			list->content_len = strlen(key);
-			list->os_brand_browser_len = strlen(value);
+			list->os_device_browser_len = strlen(value);
 			list->content = (UINT8 *)malloc((list->content_len) + 1);
-			list->os_brand_browser = (UINT8 *)malloc((list->os_brand_browser_len) + 1);
-			printf("%s,%s,%d\n", key, value, list->priority);
+			if(list->content == NULL) {
+				fprintf(stderr, "Memory allocation failed.");
+				return NULL;
+			}
+			list->os_device_browser = (UINT8 *)malloc((list->os_device_browser_len) + 1);
+			if(list->os_device_browser == NULL) {
+				fprintf(stderr, "Memory allocation failed.");
+				return NULL;
+			}
+			strncpy(list->content, key, list->content_len);
+			strncpy(list->os_device_browser, value, list->os_device_browser_len);
 		}
+
 		if(fclose(fp) != 0) {
 			fprintf(stderr, "Couldn't close the file %s\n", UA_OS_CFG);
 		}
 	}
-	if(access(UA_DEVICE_CFG, R_OK) == 0) {
-		if((fp = fopen(UA_DEVICE_CFG, "r")) == NULL) {
-			fprintf(stderr, "Couldn't open the file %s\n", UA_DEVICE_CFG);
-			return NULL;
-		}
-	}
+
+	// READ BROWSER INFO
 	if(access(UA_BROWSER_CFG, R_OK) == 0) {
 		if((fp = fopen(UA_BROWSER_CFG, "r")) == NULL) {
 			fprintf(stderr, "Couldn't open the file %s\n", UA_BROWSER_CFG);
 			return NULL;
 		}
+
+		char key[256];
+		char value[256];
+
+		while(fgets(line, 256, fp) != NULL) {
+			// 初始化一个链表节点,指向头结点head
+			if(++count == 1) {
+				list = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+				if(list == NULL) {
+					fprintf(stderr, "Memory allocation failed.");
+					return NULL;
+				}
+				head = list;
+			}
+			else {
+				tmpt = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+				if(tmpt == NULL) {
+					fprintf(stderr, "Memory allocation failed.");
+					return NULL;
+				}
+				list->next = tmpt;
+				tmpt->next = NULL;
+				list = tmpt;
+			}
+
+			sscanf(line, "%[^,]%*[,]%[^,]%*[,]%d", key, value, &list->priority);	// 读取出格式化数据
+
+			list->type = UA_BROWSER_INFO;
+			list->content_len = strlen(key);
+			list->os_device_browser_len = strlen(value);
+			list->content = (UINT8 *)malloc((list->content_len) + 1);
+			if(list->content == NULL) {
+				fprintf(stderr, "Memory allocation failed.");
+				return NULL;
+			}
+			list->os_device_browser = (UINT8 *)malloc((list->os_device_browser_len) + 1);
+			if(list->os_device_browser == NULL) {
+				fprintf(stderr, "Memory allocation failed.");
+				return NULL;
+			}
+			strncpy(list->content, key, list->content_len);
+			strncpy(list->os_device_browser, value, list->os_device_browser_len);
+		}
+
+		if(fclose(fp) != 0) {
+			fprintf(stderr, "Couldn't close the file %s\n", UA_OS_CFG);
+		}
 	}
 
+	// READ DEVICE INFO
+	if(access(UA_DEVICE_CFG, R_OK) == 0) {
+		if((fp = fopen(UA_DEVICE_CFG, "r")) == NULL) {
+			fprintf(stderr, "Couldn't open the file %s\n", UA_DEVICE_CFG);
+			return NULL;
+		}
+		char key[256];
+		char value[256];
+		char value1[64], value2[64], value3[64];	// 品牌 型号 终端类型
+		const char *terminal_types[] = {
+			"mobile", "television", "tablet", "ereader",
+			"desktop", "gaming", "media"
+		};
+
+		while(fgets(line, 256, fp) != NULL) {
+			// 初始化一个链表节点,指向头结点head
+			if(++count == 1) {
+				list = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+				if(list == NULL) {
+					fprintf(stderr, "Memory allocation failed.");
+					return NULL;
+				}
+				head = list;
+			}
+			else {
+				tmpt = (OS_DEVICE_BROWSER *)malloc(sizeof(OS_DEVICE_BROWSER));
+				if(tmpt == NULL) {
+					fprintf(stderr, "Memory allocation failed.");
+					return NULL;
+				}
+				list->next = tmpt;
+				tmpt->next = NULL;
+				list = tmpt;
+			}
+
+			sscanf(line, "%[^,]%*[,]%[^,]%*[,]%[^,]%*[,]%[^,]%*[,]", key, value1, value2, value3);	// 读取出格式化数据, key,value1, value2, value3
+
+			// 对value3,即terminal字段进行替换
+			int i;
+			for(i = 0; i < 7; i++) {
+				if(strncmp(value3, terminal_types[i], strlen(terminal_types[i])) == 0) {
+					snprintf(value, 256, "%s,%s,%d", value1, value2, i+1);
+				}
+			}
+
+			list->type = UA_DEVICE_INFO;
+			list->os_device_browser_len = strlen(value);
+			list->content_len = strlen(key);
+			list->content = (UINT8 *)malloc((list->content_len) + 1);
+			if(list->content == NULL) {
+				fprintf(stderr, "Memory allocation failed.");
+				return NULL;
+			}
+			list->os_device_browser = (UINT8 *)malloc(list->os_device_browser_len + 1);
+			if(list->os_device_browser == NULL) {
+				fprintf(stderr, "Memory allocation failed.");
+				return NULL;
+			}
+			strncpy(list->content, key, list->content_len);
+			strncpy(list->os_device_browser, value, list->os_device_browser_len);
+		}
+
+		if(fclose(fp) != 0) {
+			fprintf(stderr, "Couldn't close the file %s\n", UA_OS_CFG);
+		}
+	}
 	
-	return list;
+	return head;
+}
+
+
+int  free_os_device_browser(OS_DEVICE_BROWSER *head)
+{
+	OS_DEVICE_BROWSER *list, *deleted;
+	list = head;
+	while(list != NULL) {
+		free(list->content);
+		free(list->os_device_browser);
+		deleted = list;
+		list = list->next;
+		free(deleted);
+	}
+	
+	return 0;
 }
 
 int main()
 {
-	OS_DEVICE_BROWSER *l = NULL;
-	l = init_os_device_browser();
+	OS_DEVICE_BROWSER *list, *head;
 
+	head = init_os_device_browser();
+
+	for(list = head; list != NULL; list = list->next) {
+		// type:content_len:os_device_browser_len---content,os_device_browser,priority
+		printf("%d:%d:%d---%s,%s,%d\n", list->type, list->content_len, list->os_device_browser_len, list->content, list->os_device_browser, list->priority);
+	}
+
+	free_os_device_browser(head);
+	
 	return 0;
 }
